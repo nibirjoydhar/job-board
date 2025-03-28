f3e27e752e0f8856771898d9eca50e0a9c5ec97e
nibirjoydhar <nibirjoydhar@gmail.com>
2025-03-28 21:42:25 +0600

    ... Updated

--- html/footer.php
+++ html/footer.php
@@ -1,10 +1,10 @@
 <div class="mt-5"></div>
-<footer>
+<footer class="bg-light py-4">
     <div class="container">
         <div class="row">
+            
             <!-- Quick Links -->
-
-            <div class="col-md-4" style="border: 5px solid green;">
+            <div class="col-md-4 p-3">
                 <h5>Quick Links</h5>
                 <ul class="list-unstyled">
                     <li><a href="index.php">Home</a></li>
@@ -14,39 +14,45 @@
                     <li><a href="privacy.php">Privacy Policy</a></li>
                 </ul>
             </div>
-
+            
             <!-- Contact Information -->
-            <div class="col-md-4" style="border: 5px solid red;">
+            <div class="col-md-4 p-3">
                 <h5>Contact Us</h5>
                 <ul class="list-unstyled">
-                    <li><i class="fas fa-map-marker-alt"></i> 9-10, Chittaranjan Avenue, Dhaka 1100</li>
-                    <li><i class="fas fa-phone"></i> +88 015-215 46883</li>
-                    <li><i class="fas fa-envelope"></i> info@jobboard.com</li>
+                    <li><i class="fas fa-map-marker-alt me-2"></i>9-10, Chittaranjan Avenue, Dhaka 1100</li>
+                    <li><i class="fas fa-phone me-2"></i>+88 015-215 46883</li>
+                    <li><i class="fas fa-envelope me-2"></i><a href="mailto:info@jobboard.com">info@jobboard.com</a></li>
                 </ul>
             </div>
-
+            
             <!-- Social Media -->
-            <div class="col-md-3" style="border: 5px solid blue;">
+            <div class="col-md-3 p-3">
                 <h5>Follow Us</h5>
-                <div class="social-icons">
-                    <a href="https://www.facebook.com/whhooami" target="_blank"><i class="fab fa-facebook"></i></a>
-                    <a href="https://x.com/NibirJoydhar" target="_blank"><i class="fab fa-twitter"></i></a>
-                    <a href="https://www.linkedin.com/in/nibirjoydhar" target="_blank"><i
-                            class="fab fa-linkedin"></i></a>
-                    <a href="https://www.instagram.com/nibirjoydhar/" target="_blank"><i
-                            class="fab fa-instagram"></i></a>
-                    <a href="https://www.github.com/nibirjoydhar/" target="_blank"><i
-                            class="fab fa-github"></i></a>
+                <div class="social-icons d-flex gap-3">
+                    <a href="https://www.facebook.com/whhooami" target="_blank" class="text-dark">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="https://x.com/NibirJoydhar" target="_blank" class="text-dark">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/nibirjoydhar" target="_blank" class="text-dark">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                    <a href="https://www.instagram.com/nibirjoydhar/" target="_blank" class="text-dark">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="https://www.github.com/nibirjoydhar/" target="_blank" class="text-dark">
+                        <i class="fab fa-github"></i>
+                    </a>
                 </div>
             </div>
-
         </div>
     </div>
-
+    
     <!-- Footer Bottom -->
-    <div class="footer-bottom">
+    <div class="footer-bottom text-center mt-3 py-2 bg-dark text-white">
         <div class="container">
-            <p>&copy; 2025 Job Board. All Rights Reserved.</p>
+            <p class="mb-0">&copy; 2025 Job Board. All Rights Reserved.</p>
         </div>
     </div>
 </footer>
